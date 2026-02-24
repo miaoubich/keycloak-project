5faf83e48ecd45cc57d8c4efbf48278d6e301406
Ali Bouzar <abouzar@optimizerx.com>
2026-02-24 12:13:09 +0100

    implementing Transactional annotation at the service level

--- src/main/java/com/miaoubich/param/ParamServiceImpl.java
+++ src/main/java/com/miaoubich/param/ParamServiceImpl.java
@@ -1,6 +1,7 @@
 package com.miaoubich.param;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ParamServiceImpl implements ParamService {
@@ -11,17 +12,20 @@ public class ParamServiceImpl implements ParamService {
         this.paramRepository = paramRepository;
     }
 
+    @Transactional
     @Override
     public void saveParamValue(Param parameter) {
         paramRepository.save(parameter);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public Param getParamById(Long id) {
         return paramRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Param not found: " + id));
     }
 
+    @Transactional
     @Override
     public Param updateParamValue(Param param) {
         Param existingParam = getParamById(param.getId());
@@ -31,12 +35,14 @@ public class ParamServiceImpl implements ParamService {
         return paramRepository.save(existingParam);
     }
 
+    @Transactional
     @Override
     public void deleteParamById(Long id) {
         Param existingParam = getParamById(id);
         paramRepository.delete(existingParam);
     }
 
+    @Transactional
     @Override
     public void deleteAll() {
         paramRepository.deleteAll();
